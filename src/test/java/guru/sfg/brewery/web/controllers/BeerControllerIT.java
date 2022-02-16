2e352ac21103614ed729be13d8b43edcd6b30065
V Doshen <8881213-doshen.vv@users.noreply.gitlab.com>
2022-02-16 23:02:40 +0300

    test with HttpBasic(not just security logic, but alsoauth logic is tested)

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -58,4 +59,13 @@ public class BeerControllerIT {
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
     }
+
+    @Test
+    void findBeersWithHttpBasic() throws Exception{
+        mockMvc.perform(get("/beers/find").with(httpBasic("spring","guru")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
 }
